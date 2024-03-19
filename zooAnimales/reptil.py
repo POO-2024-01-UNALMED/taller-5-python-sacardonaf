6b1a9cf88e27bc7cf1d6c9ffcf618b187bc485b8
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-19 16:14:01 -0500

    Update and rename Reptil.py to reptil.py

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -4,40 +4,39 @@ class Reptil(Animal):
     _listado = []
     iguanas = 0
     serpientes = 0
-
+    
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
         super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
         self._largoCola = largoCola
         Reptil._listado.append(self)
 
+    @staticmethod
+    def cantidadReptiles():
+        return len(Reptil._listado)
+    
+    @staticmethod
+    def movimiento():
+        return "reptar"
+    
+    @classmethod
+    def crearIguana(cls, nombre, edad, genero):
+       cls.iguanas += 1 
+       return cls(nombre, edad, "humedal", genero, "verde", 3)
+
+    @classmethod
+    def crearSerpiente(cls, nombre, edad, genero):
+        cls.serpientes += 1 
+        return cls(nombre, edad, "jungla", genero, "blanco", 1)
+
     def getColorEscamas(self):
         return self._colorEscamas
-    
+
     def setColorEscamas(self, colorEscamas):
         self._colorEscamas = colorEscamas
 
     def getLargoCola(self):
         return self._largoCola
     
-    def setLargoPluma(self, largoCola):
+    def setLargoCola(self, largoCola):
         self._largoCola = largoCola
-
-    def movimiento(self):
-        return "reptar"
-
-    @classmethod
-    def cantidadReptiles(cls):
-        return len(Reptil._listado)
-    
-    @classmethod
-    def crearIguana(cls, nombre, edad, genero):
-        iguana = Reptil(nombre, edad, "humedal", genero, "verde", 3)
-        Reptil.iguanas += 1
-        return iguana
-
-    @classmethod
-    def crearSerpiente(cls, nombre, edad, genero):
-        serpiente = Reptil(nombre, edad, "jungla", genero, "blanco", 1)
-        Reptil.serpientes += 1
-        return serpiente

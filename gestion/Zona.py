ae6a5ea2bf628990f3267b9da469f4283f5aa607
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-19 16:04:24 -0500

    Update Zona.py

--- gestion/Zona.py
+++ gestion/Zona.py
@@ -1,12 +1,13 @@
-class Zona:
+class Zona():
+
     def __init__(self, nombre, zoo = None):
         self._nombre = nombre
         self._zoo = zoo
         self._animales = []
-
+    
     def agregarAnimales(self, animal):
         self._animales.append(animal)
-
+    
     def cantidadAnimales(self):
         return len(self._animales)
     
@@ -21,11 +22,3 @@ class Zona:
     
     def setZoo(self, zoo):
         self._zoo = zoo
-
-    def getAnimales(self):
-        return self._animales
-    
-    def setAnimales(self, animales):
-        self._animales = animales
-
-    

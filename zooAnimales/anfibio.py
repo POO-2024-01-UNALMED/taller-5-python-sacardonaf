f80e55d939b2f8a433fb5a970d6a8f7f9dd989cd
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-19 16:11:21 -0500

    Update and rename Anfibio.py to anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -4,16 +4,34 @@ class Anfibio(Animal):
     _listado = []
     ranas = 0
     salamandras = 0
-
+    
     def __init__(self, nombre, edad, habitat, genero, colorPiel, venenoso):
         super().__init__(nombre, edad, habitat, genero)
         self._colorPiel = colorPiel
         self._venenoso = venenoso
         Anfibio._listado.append(self)
 
+    @staticmethod
+    def cantidadAnfibios():
+        return len(Anfibio._listado)
+    
+    @staticmethod
+    def movimiento():
+        return "saltar"
+    
+    @classmethod
+    def crearRana(cls, nombre, edad, genero):
+        cls(nombre, edad, "selva", genero, "rojo", True)
+        cls.ranas += 1 
+
+    @classmethod
+    def crearSalamandra(cls, nombre, edad, genero):
+        cls(nombre, edad, "selva", genero, "negro y amarillo", False)
+        cls.salamandras += 1 
+
     def getColorPiel(self):
         return self._colorPiel
-    
+
     def setColorPiel(self, colorPiel):
         self._colorPiel = colorPiel
 
@@ -22,22 +40,3 @@ class Anfibio(Animal):
     
     def setVenenoso(self, venenoso):
         self._venenoso = venenoso
-
-    def movimiento(self):
-        return "saltar"
-
-    @classmethod
-    def cantidadAnfibios(cls):
-        return len(Anfibio._listado)
-    
-    @classmethod
-    def crearRana(cls, nombre, edad, genero):
-        rana = Anfibio(nombre, edad, "selva", genero, "rojo", True)
-        Anfibio.ranas += 1
-        return rana
-
-    @classmethod
-    def crearSalamandra(cls, nombre, edad, genero):
-        salamandra = Anfibio(nombre, edad, "selva", genero, "negro y amarillo", False)
-        Anfibio.salamandras += 1
-        return salamandra

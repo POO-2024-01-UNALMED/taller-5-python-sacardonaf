ec7693e6e3acb712e442e9b0de11aebf22918e3d
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-19 16:13:33 -0500

    Update and rename Pez.py to pez.py

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -4,16 +4,34 @@ class Pez(Animal):
     _listado = []
     salmones = 0
     bacalaos = 0
-
+    
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
         super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
         self._cantidadAletas = cantidadAletas
         Pez._listado.append(self)
 
+    @staticmethod
+    def cantidadPeces():
+        return len(Pez._listado)
+    
+    @staticmethod
+    def movimiento():
+        return "volar"
+    
+    @classmethod
+    def crearSalmon(cls, nombre, edad, genero):
+        cls.salmones += 1 
+        return cls(nombre, edad, "oceano", genero, "rojo", 6)
+
+    @classmethod
+    def crearBacalao(cls, nombre, edad, genero):
+        cls.bacalaos += 1 
+        return cls(nombre, edad, "oceano", genero, "gris", 6)
+
     def getColorEscamas(self):
         return self._colorEscamas
-    
+
     def setColorEscamas(self, colorEscamas):
         self._colorEscamas = colorEscamas
 
@@ -22,22 +40,3 @@ class Pez(Animal):
     
     def setCantidadAletas(self, cantidadAletas):
         self._cantidadAletas = cantidadAletas
-
-    def movimiento(self):
-        return "nadar"
-
-    @classmethod
-    def cantidadPeces(cls):
-        return len(Pez._listado)
-    
-    @classmethod
-    def crearSalmon(cls, nombre, edad, genero):
-        salmon = Pez(nombre, edad, "oceano", genero, "rojo", 6)
-        Pez.salmones += 1
-        return salmon
-
-    @classmethod
-    def crearBacalao(cls, nombre, edad, genero):
-        bacalo = Pez(nombre, edad, "oceano", genero, "gris", 6)
-        Pez.bacalaos += 1
-        return bacalo
